b6d1f164dcbb2689d7bf54839d99c75d38028c92
David de Almeida Silva <t029228@embasanet.ba.gov.br>
2025-04-11 15:21:48 -0300

    fix: simplify build configuration for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'static',
   transpilePackages: ['@chakra-ui/react', '@emotion/react', '@emotion/styled', 'react-calendar'],
   images: {
     unoptimized: true
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-    return config;
   }
 }
 
